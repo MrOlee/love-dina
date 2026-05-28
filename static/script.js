e17f4ad728b7a5a53736a3de5b6c958d107eab10
Mr Ole <72989100+MrOlee@users.noreply.github.com>
2026-05-28 23:46:35 +0700

    Update love message in showLove function

--- static/script.js
+++ static/script.js
@@ -1,5 +1,51 @@
+// COUNTDOWN
+
+const startDate = new Date("2024-01-01");
+
+function updateTimer(){
+
+  const now = new Date();
+
+  const diff = now - startDate;
+
+  const days = Math.floor(diff / (1000*60*60*24));
+
+  document.getElementById("timer").innerHTML =
+  days + " Hari Bersama ❤️";
+}
+
+setInterval(updateTimer,1000);
+
+// BUTTON LOVE
+
 function showLove(){
 
   document.getElementById("loveText").innerHTML =
-  "💖 Aku Sayang Kamu Selamanya 💖";
+  "💖 Aku Akan Selalu Menyayangimu 💖";
+
+  createHearts();
+}
+
+// HEART EFFECT
+
+function createHearts(){
+
+  for(let i=0;i<20;i++){
+
+    let heart = document.createElement("div");
+
+    heart.innerHTML = "❤️";
+
+    heart.style.position = "fixed";
+    heart.style.left = Math.random()*100+"vw";
+    heart.style.top = "100vh";
+    heart.style.fontSize = "30px";
+    heart.style.animation = "float 4s linear";
+
+    document.body.appendChild(heart);
+
+    setTimeout(()=>{
+      heart.remove();
+    },4000);
+  }
 }
